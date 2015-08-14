cbcd8198306004b721d1ae1cf46b6138c52c87f8
hao <hao_chen@partech.com>
2015-08-14 17:09:55 -0400

    1

--- angular-validation.directives.js
+++ angular-validation.directives.js
@@ -1,7 +1,15 @@
 (function(){
 	'use strict';
 
-	angular.module('angular-validation').directive('validate',['$compile',validate]);
+	angular.module('angular-validation')
+	.controller('myctrl', function($scope){
+		console.log('myctrl');
+		console.log($scope.myform);
+				console.log($scope.myform.$dirty);
+				console.dir($scope.myform.$$parentform);
+		console.dir($scope.myform.hello);
+	})
+	.directive('validate',['$compile',validate]);
 
 	function validate($compile){
 		return{
@@ -20,10 +28,14 @@
 
 						var element= iElement[0];
 						var parentDiv = element.parentNode;
-						var compiled=$compile('<span ng-show="' +iAttrs.ngModel + '">asda {{' + iAttrs.ngModel +' }}</span>');
-						var contentTr = angular.element(compiled(scope));
-						parentDiv.insertBefore(contentTr[0],element.nextSibling);
 
+						var compiled=$compile('<span ng-show="myform.' +iAttrs.name + '.$invalid">asda {{' + iAttrs.ngModel +' }}</span>');
+						var contentTr = angular.element(compiled(scope));
+						var compiled2=$compile('<div >xxxxxxxxxxxxxxx</div>');
+						var contentTr2 = angular.element(compiled2(scope));
+						var sib=element.nextSibling;
+						parentDiv.insertBefore(contentTr[0],sib);
+						parentDiv.insertBefore(contentTr2[0],sib);
 
 					},
 					post: function postLink(scope, iElement, iAttrs, controller) { 

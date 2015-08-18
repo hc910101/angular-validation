37447c72fa3da273cd7871e68ae0d670129cdf41
hao <hao_chen@partech.com>
2015-08-18 16:00:15 -0400

    ADDED functions: the first invalid element with the hint attribute will get the focus when submit button is pressed

--- angular-validation.directives.js
+++ angular-validation.directives.js
@@ -1,55 +1,47 @@
+/*
+* Usage: <input type="text" hint="Please enter a integer" ng-pattern="/^\d+$/"/>
+*
+* what it does: :
+* 			1. the input elements with the hint attribute will be set to $dirty when submit button is pressed
+*           2. the first invalid element with the hint attribute will get the focus when submit button is pressed  
+*			3. a <span> element containing the string value of the hint will be attached to the end of <input>
+*/         
 (function(){
 	'use strict';
-
-	angular.module('angular-validation')
-	.controller('myctrl', function($scope){
-		console.log('myctrl');
-		console.log($scope.myform);
-				console.log($scope.myform.$dirty);
-				console.dir($scope.myform.$$parentform);
-		console.dir($scope.myform.hello);
-	})
-	.directive('validate',['$compile',validate]);
-
-	function validate($compile){
+	var app=angular.module('form-hint');
+	app.directive('hint',['$compile','$parse','$timeout',validate]);
+	function validate($compile,$parse,$timeout){
 		return{
-
 			restrict:'A',
-
-			link:function(scope, element, attrs, controllers){
-				//console.log(element);
-				//console.log(scope);
-				//console.log(attrs);
-
-			},
 			compile: function compile(tElement, tAttrs, transclude) {
 				return {
 					pre: function preLink(scope, iElement, iAttrs, controller) { 
-
+						scope.focused=false;
+						scope.logerror=function(){
+							alert("passed");
+							return "hello";
+						};
 						var element= iElement[0];
 						var parentDiv = element.parentNode;
-
-						var compiled=$compile('<span ng-show="myform.' +iAttrs.name + '.$invalid">asda {{' + iAttrs.ngModel +' }}</span>');
-						var contentTr = angular.element(compiled(scope));
-						var compiled2=$compile('<div >xxxxxxxxxxxxxxx</div>');
-						var contentTr2 = angular.element(compiled2(scope));
+						var errorMsg = angular.element($compile('<span class="form-hint-invalid" ng-show="'+iElement[0].form.name + '.' +iAttrs.name + '.$invalid && ('+iElement[0].form.name + '.' +iAttrs.name + '.$dirty || '+ iElement[0].form.name +'.$submitted)"  > {{' + iAttrs.hint +' }}</span>')(scope));
 						var sib=element.nextSibling;
-						parentDiv.insertBefore(contentTr[0],sib);
-						parentDiv.insertBefore(contentTr2[0],sib);
-
+						parentDiv.insertBefore(errorMsg[0],sib);
+						iElement.on('submit',function(){console.log("submitted")});
+						var form=angular.element(iElement[0].form);
+							form.on('submit',function(){console.log("form submitted");
+							scope[iElement[0].form.name][iAttrs.name].$setDirty();
+							if(!scope.focused && scope[iElement[0].form.name][iAttrs.name].$invalid)
+							{
+								iElement[0].focus();
+								scope.focused=true;
+								$timeout(function(){scope.focused=false});
+							}
+						});
 					},
-					post: function postLink(scope, iElement, iAttrs, controller) { 
-					}
 				}
+			},
+		}
 
-      // or
-      // return function postLink( ... ) { ... }
-  },
-  			controller:function control($scope, $element){
-				console.log('controller');
-      			$scope.name = $scope.name + "Second ";
-    		}
+	};
 }
-
-};
-})()
+)()
--- angular-validation.module.js
+++ angular-validation.module.js
@@ -1,5 +1,4 @@
 (function(){
 	'use strict';
-	angular.module('angular-validation',[]);
-
+	angular.module('form-hint',[]);
 })()
